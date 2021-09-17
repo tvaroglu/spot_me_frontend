c405aec5cd9a9ff6c3af78b177564705e4e82943
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-09-16 22:22:44 -0500

    WIP / can receive requestt

--- app/services/back_end_service.rb
+++ app/services/back_end_service.rb
@@ -1,29 +1,47 @@
 class BackEndService
   class << self
-    def base_url
-      'http://localhost:3000/api/v1'
-      # 'https://spotme-app-api.herokuapp.com/api/v1'
-    end
 
     def get_user(google_id)
-      receive_request("/users/#{google_id}")
+      response = db_conn.get("/users/#{google_id}")
+      require "pry"; binding.pry
+      get_json(response)
     end
+
+    def base_url
+      'https://spotme-app-api.herokuapp.com/api/v1'
+    end
+
+    # def get_user(google_id)
+    #   receive_request("/users/#{google_id}")
+    # end
 
     def create_user(user_params)
-      send_request('/users', user_params)
+      db_conn.post(
+        "/users",
+        user_params.to_json,
+        'Content-Type' => 'application/json'
+      )
     end
 
-    def receive_request(uri)
-      conn = Faraday.new(url: base_url) do |faraday|
-        # faraday.headers['Authorization'] = ENV['bearer']
-      end.get(uri)
-      JSON.parse(conn.body, symbolize_names: true)
-    end
+    # def receive_request(uri)
+    #   conn = Faraday.new(url: base_url) do |faraday|
+    #     # faraday.headers['Authorization'] = ENV['bearer']
+    #   end.get(uri)
+    #   JSON.parse(conn.body, symbolize_names: true)
+    # end
 
     def send_request(uri, params)
       Faraday.new(url: base_url) do |faraday|
         faraday.headers['Content-Type'] = 'application/json'
       end.post("#{base_url}#{uri}", params.to_json)
+    end
+
+    def db_conn
+      Faraday.new(base_url)
+    end
+
+    def get_json(response)
+      JSON.parse(response.body, symbolize_names: true)
     end
   end
 end
--- spec/services/back_end_service_spec.rb
+++ spec/services/back_end_service_spec.rb
@@ -2,21 +2,22 @@ require 'rails_helper'
 
 RSpec.describe BackEndService do
   it 'can retrieve the base url for API calls' do
-    expect(BackEndService.base_url).to eq 'http://localhost:3000/api/v1'
+    expect(BackEndService.base_url).to eq 'https://spotme-app-api.herokuapp.com/api/v1'
   end
 
-  xit 'can receive a request' do
+  it 'can receive a request' do
     json_response = File.read('spec/fixtures/user.json')
-      stub_request(:get, "/users/12345").
+
+    stub_request(:get, "https://spotme-app-api.herokuapp.com/api/v1/users/789").
       with(
         headers: {
           'Accept'=>'*/*',
           'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
           'User-Agent'=>'Faraday v1.7.0'
           }).
-      to_return(status: 200, body: json_response, headers: {})
+          to_return(status: 200, body: JSON.parse(json_response), headers: {})
 
 
-    expect(BackEndService.get_user(12345).class).to eq(Hash)
+    expect(BackEndService.get_user(789).class).to eq(Hash)
   end
 end

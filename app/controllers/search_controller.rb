1d3bd4b4fe9da85006f6a21fda46f1c412497f06
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-21 19:23:24 -0400

    Create show action in controller with facade method to call service
    Co-Authored-By: Caroline Tan <carolinectan@users.noreply.github.com>

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,4 +2,8 @@ class SearchController < ApplicationController
   def index
     @gyms = BackEndFacade.get_gyms_near_user(params['zip_code'])
   end
+
+  def show
+    @gym = BackEndFacade.get_selected_gym(params[:id])
+  end
 end

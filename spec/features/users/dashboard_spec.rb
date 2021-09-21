0f3d6f340ff651cc988529b5fd5dfa006bc80759
Caroline Tan <carolinectan3@gmail.com>
2021-09-20 23:19:20 -0400

    Add test for find gyms near me button

--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 RSpec.describe 'user dashboard' do
-  it "can display the user's attributes, friends, and gyms", :vcr do
+  before :each do
     visit dashboard_path(@user.id)
-    # save_and_open_page
+  end
+
+  it "can display the user's attributes, friends, and gyms", :vcr do
     expect(page).to have_content @user.full_name
     expect(page).to have_content @user.zip_code
 
@@ -16,5 +18,15 @@ RSpec.describe 'user dashboard' do
     expect(page).to have_css '#friend' # , count: 3)
 
     expect(page).to have_link 'View Profile'
+  end
+
+  it 'has a Find Gyms Near me button', :vcr do
+    expect(page).to have_link('Find Gyms Near Me')
+  end
+
+  it 'can find gyms near me', :vcr do
+    click_on 'Find Gyms Near Me'
+
+    expect(current_path).to eq('/gym_search')
   end
 end

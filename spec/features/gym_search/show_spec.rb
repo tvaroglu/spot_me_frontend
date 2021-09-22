a1c54edcfa8811e5c7fac3b82688a7c3cb29a2b7
Caroline Tan <carolinectan3@gmail.com>
2021-09-22 17:03:36 -0400

    Add to have_css tests to gym search show spec
    Co-Authored-By: Brian Fletcher <bfl3tch@users.noreply.github.com>

--- spec/features/gym_search/show_spec.rb
+++ spec/features/gym_search/show_spec.rb
@@ -9,6 +9,9 @@ RSpec.describe 'gyms show page' do
     expect(page).to have_content("Rishi's Community Yoga")
     expect(page).to have_content('2750 S Wadsworth Blvd, Bldg D, Ste 202, Denver, CO 80227')
     expect(page).to have_content('(303) 601-0789')
+
+    expect(page).to have_css('h1', count: 1) # gym name
+    expect(page).to have_css('p', count: 2) # gym address and phone
   end
 
   it 'has a button to add the gym membership', :vcr do

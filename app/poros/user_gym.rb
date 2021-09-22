e4a99241b338474b8c88d4555e5cac0da1dc291f
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-21 22:30:29 -0400

    Update poro for correct attribute display
    Co-Authored-By: Caroline Tan <carolinectan@users.noreply.github.com>

--- app/poros/user_gym.rb
+++ app/poros/user_gym.rb
@@ -1,7 +1,9 @@
 class UserGym
-  attr_reader :name
+  attr_reader :user_id, :yelp_gym_id, :gym_name
 
   def initialize(info)
-    @name = info[:name]
+    @user_id = info[:user_id]
+    @yelp_gym_id = info[:yelp_gym_id]
+    @gym_name = info[:gym_name]
   end
 end

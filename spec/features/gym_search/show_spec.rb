aabf4d05f41bc0a91e0648371e1b5698d92f8ca5
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-21 22:28:01 -0400

    Create new feature test for creating a new gym membership via show page
    Co-Authored-By: Caroline Tan <carolinectan@users.noreply.github.com>

--- spec/features/gym_search/show_spec.rb
+++ spec/features/gym_search/show_spec.rb
@@ -10,6 +10,21 @@ RSpec.describe 'gyms show page' do
     expect(page).to have_content('2750 S Wadsworth Blvd, Bldg D, Ste 202, Denver, CO 80227')
     expect(page).to have_content('(303) 601-0789')
   end
+
+  it 'has a button to add the gym membership', :vcr do
+    expect(page).to have_button('Add Gym')
+  end
+
+  it 'actually adds the gym membership when you click that button', :vcr do
+    visit dashboard_path(@user.id)
+    expect(page).to_not have_content("Rishi's Community Yoga")
+
+    visit gym_path('BJBXzKYxQAXZKb5W6HrRnA')
+    click_on 'Add Gym'
+
+    expect(current_path).to eq(dashboard_path(@user.id))
+    expect(page).to have_content("Rishi's Community Yoga")
+  end
 end
 
 

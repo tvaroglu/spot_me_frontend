f6bd55c29fbfd137a9a79f42a0390565ce2ef9f1
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-21 22:29:06 -0400

    Create new service spec with stubbed response
    Co-Authored-By: Caroline Tan <carolinectan@users.noreply.github.com>

--- spec/services/back_end_service_spec.rb
+++ spec/services/back_end_service_spec.rb
@@ -46,4 +46,12 @@ RSpec.describe BackEndService do
     result = BackEndService.get_one_gym('BJBXzKYxQAXZKb5W6HrRnA')
     expect(result.class).to eq(Hash)
   end
+
+  it 'can create a gym membership', :vcr do
+    json_response = File.read('./spec/fixtures/gym_membership.json')
+
+    allow(BackEndService).to receive(:post_gym_membership).and_return(JSON.parse(json_response, symbolize_names: true))
+
+    expect(BackEndService.post_gym_membership(json_response).class).to eq(Hash)
+  end
 end

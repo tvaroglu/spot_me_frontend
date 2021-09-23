065224ffaee13a9d5bd0e5627e3de113df2d1546
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-23 00:48:14 -0400

    Gyms controller show action instance vars
    Co-Authored-By: Caroline Tan <carolinectan@users.noreply.github.com>

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -3,6 +3,19 @@ class GymsController < ApplicationController
     @gyms = BackEndFacade.get_gyms_near_user(params['zip_code'])
   end
 
+  def show
+    @gym = BackEndFacade.get_selected_gym(params[:id])
+    @gym_users = BackEndFacade.get_gym_users(params[:id])
+    @gym_users_count = BackEndFacade.get_gym_users_count(params[:id])
+    @gym_friends = BackEndFacade.get_friends_at_gym(params[:id])
+    @gym_non_friends = BackEndFacade.get_non_friends_at_gym(params[:id])
+  end
+
+  def create
+    BackEndFacade.create_gym_membership(gym_membership_params)
+    redirect_to dashboard_path(current_user.id)
+  end
+
   def destroy
     BackEndFacade.delete_gym_membership(gym_membership_destroy_params)
     redirect_to dashboard_path(current_user.id)
@@ -10,6 +23,10 @@ class GymsController < ApplicationController
   end
 
   private
+
+  def gym_membership_params
+    params.permit(:user_id, :yelp_gym_id, :gym_name)
+  end
 
   def gym_membership_destroy_params
     {

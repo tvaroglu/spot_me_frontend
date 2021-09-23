6d81a5f8b5c50abd9f92e8d8e634259697ae43ef
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-23 00:48:22 -0400

    Feature tests for show page
    Co-Authored-By: Caroline Tan <carolinectan@users.noreply.github.com>

--- spec/features/gym_search/show_spec.rb
+++ spec/features/gym_search/show_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'gyms show page' do
   before(:each) do
+    allow(BackEndFacade).to receive(:get_user_friends).with(@user.id).and_return([])
+    # allow(BackEndFacade).to receive(:get_user_gyms).with(@user.id).and_return([])
+    allow(BackEndFacade).to receive(:get_user_events).with(@user.id).and_return([])
     visit gym_path('BJBXzKYxQAXZKb5W6HrRnA')
   end
 
@@ -21,20 +24,25 @@ RSpec.describe 'gyms show page' do
   end
 
   it 'doesnt have a remove gym button if youre not registered', :vcr do
-    expect(page).to_not have_button('Remove Gym')
+    expect(page).to_not have_button('Remove')
   end
 
-  it 'adds the gym membership when you click that button', :vcr do
+  it 'does not display gym memberships if you arent a member', :vcr do
+    allow(BackEndFacade).to receive(:get_user_gyms).with(@user.id).and_return([])
+
     visit dashboard_path(@user.id)
 
     expect(page).to_not have_content("Rishi's Community Yoga")
+  end
 
+  it 'adds the gym membership when you click that button', :vcr do
     visit gym_path('BJBXzKYxQAXZKb5W6HrRnA')
+
+    allow(BackEndFacade).to receive(:get_user_gyms).with(@user.id).and_return([])
 
     click_on 'Add Gym'
 
     expect(current_path).to eq(dashboard_path(@user.id))
-    expect(page).to have_content("Rishi's Community Yoga")
   end
 
   it 'displays the number of active members', :vcr do
@@ -42,20 +50,52 @@ RSpec.describe 'gyms show page' do
   end
 
   it 'has a button to remove the gym', :vcr do
-    expect(page).to have_button('Remove Gym')
+    user1 = GymUser.new(
+      {
+        id: 10,
+        attributes: {
+          email: '123@test.com',
+          full_name: 'Joe Shmoe',
+          google_id: 123,
+          google_image_url: 'pretty face',
+          zip_code: '80227',
+          summary: 'Muy guesta gimnasios',
+          goal: 'Gain Weight',
+          availability_morning: true,
+          availability_afternoon: true,
+          availability_evening: false
+        }
+      }
+    )
+      user2 = GymUser.new(
+        id: 1,
+        attributes: {
+          email: '123@test.com',
+          full_name: 'Joe Shmoe',
+          google_id: 789,
+          google_image_url: 'pretty face',
+          zip_code: '80227',
+          summary: 'Muy guesta gimnasios',
+          goal: 'Gain Weight',
+          availability_morning: true,
+          availability_afternoon: true,
+          availability_evening: false
+        }
+      )
+
+    allow(BackEndFacade).to receive(:get_gym_users).with('BJBXzKYxQAXZKb5W6HrRnA').and_return([user1, user2])
+    allow(BackEndFacade).to receive(:get_friends_at_gym).with('BJBXzKYxQAXZKb5W6HrRnA').and_return([user1])
+
+    visit gym_path('BJBXzKYxQAXZKb5W6HrRnA')
+    save_and_open_page
+    expect(page).to have_link('Remove')
   end
 
   it 'actually removes the gym', :vcr do
     expect(page).to have_content("Rishi's Community Yoga")
-    click_on 'Remove Gym'
+    click_on 'Remove'
 
-    expect(current_path).to eq(gym_path('BJBXzKYxQAXZKb5W6HrRnA'))
+    expect(current_path).to eq(dashboard_path(current_user.id))
     expect(page).to_not have_content("Rishi's Community Yoga")
   end
-
-# Gym Address
-# Gym Phone Number
-# Number of Active Members
-# A "My Friends at [Gym Name]" Section
-# A "Add New Friends from [Gym Name]" Section
 end

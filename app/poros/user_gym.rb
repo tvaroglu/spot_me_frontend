1c1d05d163e68d8973205727ea26587b6d9755a4
Scott Borecki <scottborecki@gmail.com>
2021-09-23 13:21:25 -0600

    Remove unused UserGym poro

--- app/poros/user_gym.rb
+++
@@ -1,9 +0,0 @@
-class UserGym
-  attr_reader :user_id, :yelp_gym_id, :gym_name
-
-  def initialize(info)
-    @user_id = info[:user_id]
-    @yelp_gym_id = info[:yelp_gym_id]
-    @gym_name = info[:gym_name]
-  end
-end

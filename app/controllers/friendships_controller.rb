143e58fb32f5b5f5ba9e7d9fbaf830bd1f03d889
Scott Borecki <scottborecki@gmail.com>
2021-09-29 11:35:41 -0600

    Update swolemate to SwoleMate in views

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,14 +1,14 @@
 class FriendshipsController < ApplicationController
   def create
     FriendshipFacade.add_friend(create_friendship_params)
-    flash[:alert] = "You've just added a Swolemate! Now go get them gains!!"
+    flash[:alert] = "You've just added a SwoleMate! Now go get them gains!!"
     redirect_to dashboard_index_path
   end
 
   def destroy
     FriendshipFacade.delete_friend(destroy_friendship_params)
     redirect_to dashboard_index_path
-    flash[:alert] = 'Swolemate removed!'
+    flash[:alert] = 'SwoleMate removed!'
   end
 
   private

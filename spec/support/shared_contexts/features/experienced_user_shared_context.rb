96b2f3c4ddfc3c1605d282c472bde90a3b72a160
Scott Borecki <scottborecki@gmail.com>
2021-09-30 15:02:15 -0600

    Create experienced user shared context

---
+++ spec/support/shared_contexts/features/experienced_user_shared_context.rb
@@ -0,0 +1,196 @@
+shared_context 'experienced user' do
+  let(:user1) do
+    User.new(
+      id: 10,
+      attributes: {
+        email: '123@test.com',
+        full_name: 'Joe Shmoe',
+        google_id: 123,
+        google_image_url: 'pretty face',
+        zip_code: '80227',
+        summary: 'Muy guesta gimnasios',
+        goal: 'Gain Weight',
+        availability_morning: true,
+        availability_afternoon: true,
+        availability_evening: false
+      }
+    )
+  end
+
+  let(:user2) do
+    User.new(
+      id: 20,
+      attributes: {
+        email: '234@test.com',
+        full_name: 'John Doe',
+        google_id: 234,
+        google_image_url: 'pretty face',
+        zip_code: '80227',
+        summary: 'Muy guesta gimnasios',
+        goal: 'Gain Weight',
+        availability_morning: true,
+        availability_afternoon: false,
+        availability_evening: true
+      }
+    )
+  end
+
+  let(:user3) do
+    User.new(
+      id: 30,
+      attributes: {
+        email: '345@test.com',
+        full_name: 'Jane Doe',
+        google_id: 345,
+        google_image_url: 'pretty face',
+        zip_code: '80227',
+        summary: 'Muy guesta gimnasios',
+        goal: 'Gain Weight',
+        availability_morning: false,
+        availability_afternoon: false,
+        availability_evening: true
+      }
+    )
+  end
+
+  let(:gym_membership1) do
+    GymMembership.new(
+      id: '1',
+      type: 'gym_membership',
+      attributes: {
+        user_id: 1,
+        yelp_gym_id: 'lex65fkcol5gfq89rymmd2',
+        gym_name: 'Kling-Wilkinson'
+      }
+    )
+  end
+
+  let(:gym_membership2) do
+    GymMembership.new(
+      id: '7',
+      type: 'gym_membership',
+      attributes: {
+        user_id: 1,
+        yelp_gym_id: '6x10s0lbnry4ivkzcjpilk',
+        gym_name: 'Konopelski, Lowe and Haley'
+      }
+    )
+  end
+
+  let(:gym_membership3) do
+    GymMembership.new(
+      id: '17',
+      type: 'gym_membership',
+      attributes: {
+        user_id: 1,
+        yelp_gym_id: 'wxaw9m796t6wdnsk53uieh',
+        gym_name: 'Funk LLC'
+      }
+    )
+  end
+
+  let(:yelp_gym1) do
+    Gym.new(
+      id: '1',
+      type: 'gym',
+      attributes: {
+        name: 'Planet Fitness',
+        address: 'address1',
+        phone: '123-123-1234'
+      }
+    )
+  end
+
+  let(:yelp_gym2) do
+    Gym.new(
+      id: '2',
+      type: 'gym',
+      attributes: {
+        name: 'Golds Gym',
+        address: 'address2',
+        phone: '234-234-2345'
+      }
+    )
+  end
+
+  let(:yelp_gym3) do
+    Gym.new(
+      id: '3',
+      type: 'gym',
+      attributes: {
+        name: '24Hour Fitness',
+        address: 'address3',
+        phone: '345-345-3456'
+      }
+    )
+  end
+
+  let(:event1) do
+    Event.new(
+      id: '1',
+      attributes: {
+        user_id: 20,
+        gym_membership_id: 1,
+        activity: 'Bodybuilding',
+        date_time: '2022-07-22T21:41:28.000Z'
+      },
+      relationships: {
+        user: {
+          meta: {
+            full_name: user2.full_name
+          }
+        }
+      }
+    )
+  end
+
+  let(:event2) do
+    Event.new(
+      id: '2',
+      attributes: {
+        user_id: 30,
+        gym_membership_id: 2,
+        date_time: '2022-08-22T21:41:28.000Z',
+        activity: 'Running'
+      },
+      relationships: {
+        user: {
+          meta: {
+            full_name: user3.full_name
+          }
+        }
+      }
+    )
+  end
+
+  let(:event3) do
+    Event.new(
+      id: '3',
+      attributes: {
+        user_id: 20,
+        gym_membership_id: 3,
+        date_time: '2022-09-22T21:41:28.000Z',
+        activity: 'Stretching'
+      },
+      relationships: {
+        user: {
+          meta: {
+            full_name: user2.full_name
+          }
+        }
+      }
+    )
+  end
+
+  let(:user_friends) { [user1, user2, user3] }
+  let(:user_gyms) { [gym_membership1, gym_membership2, gym_membership3] }
+  let(:searched_gyms) { [yelp_gym1, yelp_gym2, yelp_gym3] }
+  let(:user_events) { [event1, event2, event3] }
+
+  before do
+    allow(FriendshipFacade).to receive(:get_friends).with(user.id).and_return(user_friends)
+    allow(GymMembershipFacade).to receive(:get_gym_memberships).with(user.id).and_return(user_gyms)
+    allow(EventFacade).to receive(:get_upcoming_events).with(user.id).and_return(user_events)
+    allow(GymFacade).to receive(:get_gyms_near_user).with(user.zip_code).and_return(searched_gyms)
+  end
+end

c1fdaf0781b751f281e79336d9bd5f4bf6261342
Scott Borecki <scottborecki@gmail.com>
2021-09-30 11:55:44 -0600

    Add comments for date_format methods

--- app/poros/event.rb
+++ app/poros/event.rb
@@ -11,11 +11,13 @@ class Event
   end
 
   # TODO: Update method to handle different timezones
+  # Returns the date in the format of 'Wednesday, Sep 22, 2021  9:41pm'
   def format_date
     DateTime.strptime(date_time, '%Y-%m-%dT%H:%M:%S.000').strftime('%A, %b %d, %Y %l:%M%P')
   end
 
   # TODO: Update method to handle different timezones
+  # Returns the date in the format of 'Tue 11/30/21 3:00pm'
   def format_date_short
     DateTime.strptime(date_time, '%Y-%m-%dT%H:%M:%S.000').strftime('%a %m/%d/%y %l:%M%P')
   end

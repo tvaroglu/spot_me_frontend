74fa931ec5a3a4ac561fba7cd63c33432d238b3b
Scott Borecki <scottborecki@gmail.com>
2021-10-01 19:35:08 -0600

    Add address line attributes

--- app/poros/gym.rb
+++ app/poros/gym.rb
@@ -1,10 +1,23 @@
 class Gym
-  attr_reader :yelp_gym_id, :name, :address, :phone
+  attr_reader :yelp_gym_id, :name, :address, :address1, :address2, :address3,
+              :city_state_zip, :phone
 
   def initialize(data)
-    @yelp_gym_id = data[:id]
-    @name        = data[:attributes][:name]
-    @address     = data[:attributes][:address]
-    @phone       = data[:attributes][:phone]
+    @yelp_gym_id    = data[:id]
+    @name           = data[:attributes][:name]
+    @address        = data[:attributes][:address]
+    @address1       = data[:attributes][:address_details][:address1]
+    @address2       = data[:attributes][:address_details][:address2]
+    @address3       = data[:attributes][:address_details][:address3]
+    @city_state_zip = format_city_state_zip(data)
+    @phone          = data[:attributes][:phone]
+  end
+
+  def format_city_state_zip(data)
+    city = data[:attributes][:address_details][:city]
+    state_abbr = data[:attributes][:address_details][:state]
+    zip_code = data[:attributes][:address_details][:zip_code]
+
+    "#{city}, #{state_abbr} #{zip_code}"
   end
 end
--- spec/poros/gym_spec.rb
+++ spec/poros/gym_spec.rb
@@ -7,6 +7,13 @@ describe Gym, type: :poro do
       name = 'Planet Fitness'
       address = '123 Main St'
       phone = '123-123-1234'
+      address1 = "430 Pierre St"
+      address2 = nil
+      address3 = ''
+      city = 'Boulder'
+      state = 'CO'
+      zip_code = '80304'
+      city_state_zip = 'Boulder, CO 80304'
 
       gym_params = {
         id: yelp_gym_id,
@@ -14,7 +21,15 @@ describe Gym, type: :poro do
         attributes: {
           name: name,
           address: address,
-          phone: phone
+          phone: phone,
+          address_details: {
+            address1: address1,
+            address2: address2,
+            address3: address3,
+            city: city,
+            state: state,
+            zip_code: zip_code
+          }
         }
       }
 
@@ -27,8 +42,47 @@ describe Gym, type: :poro do
       expect(gym.name).to be_a String
       expect(gym.address).to eq(address)
       expect(gym.address).to be_a String
+      expect(gym.address1).to eq(address1)
+      expect(gym.address1).to be_a String
+      expect(gym.address2).to eq(address2)
+      expect(gym.address3).to eq(address3)
+      expect(gym.city_state_zip).to eq(city_state_zip)
+      expect(gym.city_state_zip).to be_a String
       expect(gym.phone).to eq(phone)
       expect(gym.phone).to be_a String
+    end
+  end
+
+  describe 'instance methods' do
+    describe '#city_state_zip' do
+      it 'returns the concatenated city, state, and zip' do
+        yelp_gym_id = '1'
+        name = 'Planet Fitness'
+        address = '123 Main St'
+        phone = '123-123-1234'
+        city = 'Boulder'
+        state = 'CO'
+        zip_code = '80304'
+
+        gym_params = {
+          id: yelp_gym_id,
+          type: 'gym',
+          attributes: {
+            name: name,
+            address: address,
+            phone: phone,
+            address_details: {
+              city: city,
+              state: state,
+              zip_code: zip_code
+            }
+          }
+        }
+
+        gym = Gym.new(gym_params)
+
+        expect(gym.format_city_state_zip(gym_params)).to eq('Boulder, CO 80304')
+      end
     end
   end
 end

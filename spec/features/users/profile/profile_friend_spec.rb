ba9abdd50d5a5cdadc2a4b02353b348a591ccde0
Scott Borecki <scottborecki@gmail.com>
2021-10-10 16:12:13 -0600

    Fix followers reference

--- spec/features/users/profile/profile_friend_spec.rb
+++ spec/features/users/profile/profile_friend_spec.rb
@@ -30,6 +30,7 @@ describe 'user profile page: friend', type: :feature do
       allow(UserFacade).to receive(:get_profile_user).with(user10.id.to_s).and_return(user10)
       allow(FriendshipFacade).to receive(:get_friends).with(user.id).and_return([user10])
       allow(FriendshipFacade).to receive(:get_friends).with(user10.id).and_return([])
+      allow(FriendshipFacade).to receive(:get_followers).with(user10.id).and_return([])
       allow(EventFacade).to receive(:get_events).with(user10.id).and_return([])
       allow(EventFacade).to receive(:get_events).with(user10.id, 'past').and_return([])
     end

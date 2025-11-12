7e526f079d22a23907127b20440f078504195c36
Taylor Varoglu <taylor.varoglu@gmail.com>
2025-11-11 17:10:17 -0700

    Add early return into BackEndService#parse_json and TODO comment

--- app/services/back_end_service.rb
+++ app/services/back_end_service.rb
@@ -5,6 +5,11 @@ class BackEndService
   end
 
   def self.parse_json(response)
+    # TODO: debug API; receiving 500's on certain gym membership add/removes..?
+      # i.e. NoMethodError (undefined method `address' for #<Hash:0x000055cc15d77508>):
+    return {} if response.body.nil? || response.body.strip.empty?
+    # puts "STATUS: #{response.status}"
+    # puts "BODY: #{response.body.inspect}"
     JSON.parse(response.body, symbolize_names: true)
   end
 end
